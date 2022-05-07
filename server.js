cac2ee63ed0a217d9096f4e2abc86f088ae59848
sidkhuntia <siddharthakhuntia@gmail.com>
2022-05-07 15:31:00 +0530

    login page

--- server.js
+++ server.js
@@ -6,16 +6,17 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
-// app.set("views", __dirname + "/views");
+app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
-// app.use("/public",express.static("public"));
+app.use("/public",express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false, limit: "10mb" }));
 
 
 const indexRouter = require("./routes/index");
-
+const loginRouter = require("./routes/login");
 app.use("/", indexRouter);
+app.use("/login", loginRouter)
 
 
 const port = process.env.PORT || 3000;
