f1291abb109671b1898ee1e0f051559ca4b8593e
sidkhuntia <siddharthakhuntia@gmail.com>
2022-05-10 20:04:50 +0530

    minor fixes

--- server.js
+++ server.js
@@ -42,9 +42,7 @@ const notificationRouter = require("./routes/notification");
 const loanRequestRouter = require("./routes/loanrequest");
 const profileRouter = require("./routes/profile");
 const contactUsRouter = require("./routes/contactus");
-const aboutUsRouter = require("./routes/aboutus");
 const servicesRouter = require("./routes/services");
-const termsRouter = require("./routes/terms");
 const negotiateRouter = require("./routes/negotiateLoan");
 
 var googleUser = new Object();
@@ -104,9 +102,7 @@ app.use("/notification", isLoggedIn, notificationRouter);
 app.use("/loanrequest",isLoggedIn, isProfileCreated,isEligible, loanRequestRouter);
 app.use("/profile", isLoggedIn, profileRouter);
 app.use("/contactus", contactUsRouter);
-app.use("/aboutus", aboutUsRouter);
 app.use("/services", servicesRouter);
-app.use("/terms", termsRouter);
 app.use("/negotiate",isLoggedIn,isProfileCreated, negotiateRouter);
 
 app.get("/", isLoggedIn, (req, res) => {
